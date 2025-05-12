ef3009ae1013ff990ed6947caf747b0bcb328496
CarlosOmarHdzHdz <omarc5906@gmail.com>
2025-05-12 15:51:55 -0600

    se implementa vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+// --- INICIO CÓDIGO AGREGADO ---
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// --- FIN CÓDIGO AGREGADO ---
 
 import App from './App.vue'
 import router from './router'
 
+// --- INICIO CÓDIGO AGREGADO ---
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+// --- FIN CÓDIGO AGREGADO ---
+
 const app = createApp(App)
+
+// --- INICIO CÓDIGO AGREGADO ---
+app.use(vuetify) // Asegúrate que esta línea se añada
+// --- FIN CÓDIGO AGREGADO ---
 
 app.use(createPinia())
 app.use(router)
